aeeac1b781b67b19600fda925b6bcc5ffc5b7ff9
Theuz <123218001+Theuzqrz@users.noreply.github.com>
2023-01-31 13:22:09 -0300

    Update

---
+++ modulo-d/Exercicio/script.js
@@ -0,0 +1,27 @@
+
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src="./img/dia.png"
+        document.body.style.background = '#e2cd9f';
+
+    } else if (hora >= 12 && hora < 18) {
+        // boa tarde 
+        img.src ="./img/tarde.png"
+        document.body.style.background = '#b9846f';
+
+    } else {
+        // boa noite!
+        img.src ="./img/noite.png"
+        document.body.style.background = '#515154';
+
+    }
+
+}
